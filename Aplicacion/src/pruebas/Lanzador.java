556067d322aa3eab60c5cca02d7842422359d493
Patricia Lazaro <plazaro1994@gmail.com>
2015-03-30 21:29:38 +0200

    Agregadas variables globales a las pruebas

--- Aplicacion/src/pruebas/Lanzador.java
+++ Aplicacion/src/pruebas/Lanzador.java
@@ -2,5 +2,15 @@ package pruebas;
 
 /* clase desde la que lanzar las distintas pruebas */
 public class Lanzador {
+	
+	/* variables que usar en las pruebas */
+	public final static String TITULO = "Halo";
+	public final static String URL = "img.com";
+	public final static String DESCRIPCION = "bonito";
+	public final static String LANZAMIENTO = "05/05/2005";
+	public final static String RATING = "8.0";
+	public final static String GENERO = "Shooter";
+	public final static int PRECIO = 51;
+	public final static String PLATAFORMA = "PC";
 
 }
