6bf95aeb729c5fcb0bb5c71f90a378ad62ef3987
Jaime <jai_mi_to@hotmail.es>
2015-05-26 21:54:32 +0200

    Quitado un warning

--- Aplicacion/src/controlador/BotonesInfo.java
+++ Aplicacion/src/controlador/BotonesInfo.java
@@ -17,7 +17,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
